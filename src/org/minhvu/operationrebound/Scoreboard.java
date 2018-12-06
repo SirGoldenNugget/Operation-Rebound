87663796ea1c1f4705e4ef7e939e307524e9c209
Minh Vu <minhd_vu@yahoo.com>
2018-12-05 21:02:49 -0500

    scoreboard is able to track the score and accuracy

--- src/org/minhvu/operationrebound/Scoreboard.java
+++ src/org/minhvu/operationrebound/Scoreboard.java
@@ -8,11 +8,10 @@ public abstract class Scoreboard {
     public static int bulletsHit = 0;
 
     public static void paint(Graphics2D g2d) {
-
-        int accuracy = (int) ((bulletsHit / (double) bulletsFired) * 100);
+        int accuracy = bulletsFired > 0 ? (int) ((bulletsHit / (double) bulletsFired) * 100) : 0;
 
         g2d.setColor(Color.BLACK);
         g2d.setFont(new Font("calibri", Font.BOLD, 16));
-        g2d.drawString("Score: " + score + "\tAccuracy: " + accuracy, 10, 20);
+        g2d.drawString("Score: " + score + " Accuracy: " + accuracy + "%", 10, 20);
     }
 }
